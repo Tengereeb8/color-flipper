4dc819c30ad0da4804ca880e97be578d721e9323
“Tengereeb8” <tengererdenebattulga8@gmail.com>
2025-12-24 09:56:54 +0800

    donee

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const colorChangeButton = document.getElementById("colorChanger");
 colorChangeButton.addEventListener("click", () => {
   const backgroundColor = document.querySelector(".main");
-  const color = Math.floor(Math.random() * 4) + 1;
-  if (color === 1) {
-    colorText = backgroundColor.style.backgroundColor = "#6aeb70";
+  const hexDigits = "0123456789abcdef";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hexDigits[Math.floor(Math.random() * 16)];
   }
-  if (color === 2) {
-    colorText = backgroundColor.style.backgroundColor = "#d76aeb";
-  }
-  if (color === 3) {
-    colorText = backgroundColor.style.backgroundColor = "#eb996a";
-  }
-  if (color === 4) {
-    colorText = backgroundColor.style.backgroundColor = "#6a82eb";
-  }
+
   const colorName = document.getElementById("colorName");
-  colorName.textContent = `${colorText}`;
-  colorName.style.color = `${colorText}`;
+  colorName.textContent = `${color}`;
+  colorName.style.color = `${color}`;
+  backgroundColor.style.backgroundColor = color;
 });
